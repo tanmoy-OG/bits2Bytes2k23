426225ebd7a103f1197ce599c552659f2a3d0a87
Priyankar Sarkar <priyankarsarkar2020@gmail.com>
2023-06-25 15:54:53 +0530

    navbar bug fixed - mobile view background blur about background fixed body user select turned off

--- client-app/src/About.jsx
+++ client-app/src/About.jsx
@@ -1,7 +1,7 @@
 export const About = () => {
   return (
     <div className="flex flex-col px-4 md:pl-20 md:pr-20" id="about">
-      <div className="bg-neutral-800 h-80 w-full left-0 absolute mix-blend-overlay"></div>
+      <div className="bg-neutral-800 h-full w-full left-0 absolute mix-blend-overlay"></div>
       <h1
         className="p-4 md:p-10 pb-5 text-4xl md:text-5xl font-bold tracking-wider text-neutral-200 font-custom-sans"
         data-aos="fade-up "
@@ -11,7 +11,6 @@ export const About = () => {
       <p
         className="pb-4 md:pb-10 pl-4 md:pl-20 pr-4 md:pr-20 text-base md:text-2xl leading-8 md:leading-10 text-left text-neutral-300 font-custom-sans"
         data-aos="fade-right"
-        data-aos-delay="1000"
       >
         The 16th annual "Bits2Bytes 2023" technical festival will take place at Bengal Institute of Technology in West Bengal on July 17-18, 2023. This self-financing event aims to develop psycho-motor skills, technological innovation, and creativity in young talent. Successful graduates will showcase drone creation, autonomous robotics projects, and Android-based projects. Research-based activities, such as term papers and poster presentations, will motivate students. Over 50 cutting-edge technical projects from various fields, including computer science, biotechnology, and electronics, will be showcased. The event will also include a National Symposium on Computer Science, Electronics, and Biotechnology, a Short Film Making Competition, and an Exhibition on the Use of Autonomous Robotics in Saving the Earth and Nature.
       </p>
