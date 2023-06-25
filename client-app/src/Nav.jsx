3a02e64e220bdc18ff2e4a60001c4583b7ec5a27
Priyankar Sarkar <priyankarsarkar2020@gmail.com>
2023-06-25 21:36:53 +0530

    gallery ui done tailwind config updated: window breakpoints added tab logo + title changed

--- client-app/src/Nav.jsx
+++ client-app/src/Nav.jsx
@@ -27,7 +27,7 @@ export const Nav = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <a href="/" className="text-violet-300 text-xl font-bold font-custom-sans">
-            IMPULSE 2K23
+            BITS2BYTES 2K23
           </a>
           <div className="-mr-2 flex md:hidden">
             <button
