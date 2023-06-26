2def5ccee2d896deff250e7c351ec64b5ed2cc4b
Priyankar Sarkar <priyankarsarkar2020@gmail.com>
2023-06-26 23:36:49 +0530

    1. Gallery UI fixed 2. Responsive gallery UI 3. Gallery image open on click

--- client-app/src/Homepage/Gallery.jsx
+++ client-app/src/Homepage/Gallery.jsx
@@ -1,137 +1,38 @@
 import pic1 from "../assets/download.jpeg";
 import pic2 from "../assets/tree-736885_1280.jpg";
 import pic3 from "../assets/5616.webp";
+import pic4 from "../assets/lomba.jpg";
+import pic5 from "../assets/Landscape-Color.jpg";
+import { EachGalleryItem } from "./EachGalleryItem";
 
-export const Gallery = () => {
+export const Gallery = ({setChildVisible, setImg, setTag}) => {
+  const tag = "IMAGE TAG";
+
   return (
     <div className="mb-10 p-6 bg-blue-500/10">
       <h1 className="p-4 md:p-10 pb-5 text-4xl md:text-5xl font-bold tracking-wider text-neutral-200 font-custom-sans">
         GALLERY
       </h1>
-      <div className="gallery-images grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-6" data-aos="fade-up">
 
-        <div className="w-full h-72 relative group">
-            <div className="w-full h-full overflow-hidden">
-                <img
-                src={pic1}
-                alt=""
-                className="w-full h-full object-cover grayscale group-hover:scale-110 group-hover:grayscale-0 transition-all duration-500"
-                />
-            </div>
-            <div className="absolute bottom-0 z-50 opacity-0 group-hover:opacity-100 w-4/5 h-10 bg-slate-50/30 md:text-2xl font-bold tracking-wider text-neutral-200 font-custom-sans flex justify-center items-center backdrop-blur-sm transition-all duration-500">IMAGE TAG</div>
-        </div>
-
-        <div className="w-full h-72 relative group">
-            <div className="w-full h-full overflow-hidden">
-                <img
-                src={pic2}
-                alt=""
-                className="w-full h-full object-cover grayscale group-hover:scale-110 group-hover:grayscale-0 transition-all duration-500"
-                />
-            </div>
-            <div className="absolute bottom-0 z-50 opacity-0 group-hover:opacity-100 w-4/5 h-10 bg-slate-50/30 md:text-2xl font-bold tracking-wider text-neutral-200 font-custom-sans flex justify-center items-center backdrop-blur-sm transition-all duration-500">IMAGE TAG</div>
-        </div>
-
-        <div className="w-full h-72 relative group">
-            <div className="w-full h-full overflow-hidden">
-                <img
-                src={pic3}
-                alt=""
-                className="w-full h-full object-cover grayscale group-hover:scale-110 group-hover:grayscale-0 transition-all duration-500"
-                />
-            </div>
-            <div className="absolute bottom-0 z-50 opacity-0 group-hover:opacity-100 w-4/5 h-10 bg-slate-50/30 md:text-2xl font-bold tracking-wider text-neutral-200 font-custom-sans flex justify-center items-center backdrop-blur-sm transition-all duration-500">IMAGE TAG</div>
-        </div>
-
-        <div className="w-full h-72 relative group">
-            <div className="w-full h-full overflow-hidden">
-                <img
-                src={pic1}
-                alt=""
-                className="w-full h-full object-cover grayscale group-hover:scale-110 group-hover:grayscale-0 transition-all duration-500"
-                />
-            </div>
-            <div className="absolute bottom-0 z-50 opacity-0 group-hover:opacity-100 w-4/5 h-10 bg-slate-50/30 md:text-2xl font-bold tracking-wider text-neutral-200 font-custom-sans flex justify-center items-center backdrop-blur-sm transition-all duration-500">IMAGE TAG</div>
-        </div>
-
-        <div className="w-full h-72 relative group">
-            <div className="w-full h-full overflow-hidden">
-                <img
-                src={pic3}
-                alt=""
-                className="w-full h-full object-cover grayscale group-hover:scale-110 group-hover:grayscale-0 transition-all duration-500"
-                />
-            </div>
-            <div className="absolute bottom-0 z-50 opacity-0 group-hover:opacity-100 w-4/5 h-10 bg-slate-50/30 md:text-2xl font-bold tracking-wider text-neutral-200 font-custom-sans flex justify-center items-center backdrop-blur-sm transition-all duration-500">IMAGE TAG</div>
-        </div>
-
-        <div className="w-full h-72 relative group">
-            <div className="w-full h-full overflow-hidden">
-                <img
-                src={pic2}
-                alt=""
-                className="w-full h-full object-cover grayscale group-hover:scale-110 group-hover:grayscale-0 transition-all duration-500"
-                />
-            </div>
-            <div className="absolute bottom-0 z-50 opacity-0 group-hover:opacity-100 w-4/5 h-10 bg-slate-50/30 md:text-2xl font-bold tracking-wider text-neutral-200 font-custom-sans flex justify-center items-center backdrop-blur-sm transition-all duration-500">IMAGE TAG</div>
-        </div>
-
-        <div className="w-full h-72 relative group">
-            <div className="w-full h-full overflow-hidden">
-                <img
-                src={pic3}
-                alt=""
-                className="w-full h-full object-cover grayscale group-hover:scale-110 group-hover:grayscale-0 transition-all duration-500"
-                />
-            </div>
-            <div className="absolute bottom-0 z-50 opacity-0 group-hover:opacity-100 w-4/5 h-10 bg-slate-50/30 md:text-2xl font-bold tracking-wider text-neutral-200 font-custom-sans flex justify-center items-center backdrop-blur-sm transition-all duration-500">IMAGE TAG</div>
-        </div>
-
-        <div className="w-full h-72 relative group">
-            <div className="w-full h-full overflow-hidden">
-                <img
-                src={pic1}
-                alt=""
-                className="w-full h-full object-cover grayscale group-hover:scale-110 group-hover:grayscale-0 transition-all duration-500"
-                />
-            </div>
-            <div className="absolute bottom-0 z-50 opacity-0 group-hover:opacity-100 w-4/5 h-10 bg-slate-50/30 md:text-2xl font-bold tracking-wider text-neutral-200 font-custom-sans flex justify-center items-center backdrop-blur-sm transition-all duration-500">IMAGE TAG</div>
-        </div>
-
-        <div className="w-full h-72 relative group">
-            <div className="w-full h-full overflow-hidden">
-                <img
-                src={pic2}
-                alt=""
-                className="w-full h-full object-cover grayscale group-hover:scale-110 group-hover:grayscale-0 transition-all duration-500"
-                />
-            </div>
-            <div className="absolute bottom-0 z-50 opacity-0 group-hover:opacity-100 w-4/5 h-10 bg-slate-50/30 md:text-2xl font-bold tracking-wider text-neutral-200 font-custom-sans flex justify-center items-center backdrop-blur-sm transition-all duration-500">IMAGE TAG</div>
-        </div>
-
-        <div className="w-full h-72 relative group">
-            <div className="w-full h-full overflow-hidden">
-                <img
-                src={pic3}
-                alt=""
-                className="w-full h-full object-cover grayscale group-hover:scale-110 group-hover:grayscale-0 transition-all duration-500"
-                />
-            </div>
-            <div className="absolute bottom-0 z-50 opacity-0 group-hover:opacity-100 w-4/5 h-10 bg-slate-50/30 md:text-2xl font-bold tracking-wider text-neutral-200 font-custom-sans flex justify-center items-center backdrop-blur-sm transition-all duration-500">IMAGE TAG</div>
-        </div>
-
-        <div className="w-full h-72 relative group">
-            <div className="w-full h-full overflow-hidden">
-                <img
-                src={pic1}
-                alt=""
-                className="w-full h-full object-cover grayscale group-hover:scale-110 group-hover:grayscale-0 transition-all duration-500"
-                />
-            </div>
-            <div className="absolute bottom-0 z-50 opacity-0 group-hover:opacity-100 w-4/5 h-10 bg-slate-50/30 md:text-2xl font-bold tracking-wider text-neutral-200 font-custom-sans flex justify-center items-center backdrop-blur-sm transition-all duration-500">IMAGE TAG</div>
-        </div>
-
+      <div className="grid-wrapper" data-aos="fade-up">
+        <EachGalleryItem img={pic1} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
+        <EachGalleryItem img={pic3} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
+        <EachGalleryItem img={pic2} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
+        <EachGalleryItem img={pic3} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
+        <EachGalleryItem img={pic1} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
+        <EachGalleryItem img={pic2} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
+        <EachGalleryItem img={pic2} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
+        <EachGalleryItem img={pic1} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
+        <EachGalleryItem img={pic3} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
+        <EachGalleryItem img={pic3} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
+        <EachGalleryItem img={pic1} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
+        <EachGalleryItem img={pic2} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
+        <EachGalleryItem img={pic2} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
+        <EachGalleryItem img={pic1} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
+        <EachGalleryItem img={pic3} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
       </div>
     </div>
   );
 };
+
+
