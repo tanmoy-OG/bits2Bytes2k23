6d7cbaf3b6eacbf97eddc128bdc1b6fe154a636a
Priyankar Sarkar <priyankarsarkar2020@gmail.com>
2023-06-27 13:03:25 +0530

    gallery component: css fixed eachgalleryitem component: random image size logic improved present component: margin top increased secretary component: margin padding fixed navbar component: color and transition UI fixed viewgalleryimage: UI made better

--- client-app/src/Homepage/Gallery.jsx
+++ client-app/src/Homepage/Gallery.jsx
@@ -9,8 +9,8 @@ export const Gallery = ({setChildVisible, setImg, setTag}) => {
   const tag = "IMAGE TAG";
 
   return (
-    <div className="mb-10 p-6 bg-blue-500/10">
-      <h1 className="p-4 md:p-10 pb-5 text-4xl md:text-5xl font-bold tracking-wider text-neutral-200 font-custom-sans">
+    <div className="mb-10 p-6 pt-0 bg-blue-500/10">
+      <h1 className="p-4 md:p-10 md:pb-14 text-4xl md:text-5xl font-bold tracking-wider text-neutral-200 font-custom-sans">
         GALLERY
       </h1>
 
@@ -18,16 +18,16 @@ export const Gallery = ({setChildVisible, setImg, setTag}) => {
         <EachGalleryItem img={pic1} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
         <EachGalleryItem img={pic3} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
         <EachGalleryItem img={pic2} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
-        <EachGalleryItem img={pic3} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
+        <EachGalleryItem img={pic5} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
         <EachGalleryItem img={pic1} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
         <EachGalleryItem img={pic2} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
         <EachGalleryItem img={pic2} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
-        <EachGalleryItem img={pic1} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
+        <EachGalleryItem img={pic4} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
         <EachGalleryItem img={pic3} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
-        <EachGalleryItem img={pic3} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
+        <EachGalleryItem img={pic5} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
         <EachGalleryItem img={pic1} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
         <EachGalleryItem img={pic2} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
-        <EachGalleryItem img={pic2} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
+        <EachGalleryItem img={pic4} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
         <EachGalleryItem img={pic1} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
         <EachGalleryItem img={pic3} tag={tag} setChildVisible={setChildVisible} setImg={setImg} setTag={setTag}/>
       </div>
