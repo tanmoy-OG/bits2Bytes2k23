9de903e42588ac2976e9b796afe64494d086d6cc
Tanmoy Choudhury <official.tanmoychoudhury@protonmail.com>
2023-06-30 21:09:52 +0530

    Changelog: 1. Changed the Shadow of CardHome. 2. Resolved Merge Conflicts

--- client-app/src/CardHome.jsx
+++ client-app/src/CardHome.jsx
@@ -8,7 +8,7 @@ import {
 export const Card = (props) => {
   return (
     <div data-aos="fade-up" data-aos-duration="5000" className="flex">
-      <div className="bg-teal-950 rounded-xl w-72 m-8 mb-10 p-0 pb-4 transition-all duration-500 hover:transition-all hover:duration-500 hover:bg-orange-800 hover:shadow-lg hover:p-4 hover:pb-8 group">
+      <div className="bg-teal-950 rounded-xl w-72 m-8 mb-10 p-0 pb-4 transition-all duration-500 hover:transition-all hover:duration-500 hover:bg-orange-800 shadow-lg shadow-black hover:p-4 hover:pb-8 group">
         <div className="flex pb-6 justify-center">
           <img
             src="https://demo.epic-webdesign.com/tf-pacifico/v1/images/team1a.jpg"
