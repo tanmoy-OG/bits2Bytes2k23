979dbf41f6015db6bd37a23d36424ba979335150
Tanmoy Choudhury <official.tanmoychoudhury@protonmail.com>
2023-07-02 23:51:08 +0530

    Changelog: 1. Added library react-tsparicles > npm i react-tsparticles 2. Added library vanta > npm i vanta 3. Added background Three Js animation, starry sky and Particle Js 4. Removed the Timer and enlarged the Logo 5. Gave the logo a minimal Background 6. Added more details about crew members in Cards

--- client-app/src/App.jsx
+++ client-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import "./App.css";
+import NET from "vanta/src/vanta.net";
 import Aos from "aos";
-import "aos/dist/aos.css";
+import Particle from "./Homepage/Particle";
 import { Nav } from "./Nav";
 import { Present } from "./Homepage";
 import { About } from "./Homepage";
@@ -15,7 +15,10 @@ import { MainProfile } from "./Profile/MainProfile";
 import { Principal } from "./Principal";
 import { Crew } from "./Crew";
 
-function App() {
+import "./App.css";
+import "aos/dist/aos.css";
+
+export default function App() {
   const [childVisible, setChildVisible] = useState(false);
   const [img, setImg] = useState();
   const [tag, setTag] = useState(null);
@@ -23,25 +26,44 @@ function App() {
 
   useEffect(() => {
     Aos.init({ duration: 1300 });
+    NET({
+      el: "#vanta",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.0,
+      minWidth: 200.0,
+      scale: 1.0,
+      scaleMobile: 1.0,
+      color: 0xe708e,
+      backgroundColor: 0x0,
+      maxDistance: 1.0,
+      spacing: 12.0,
+    });
   }, []);
 
   return (
     <div className="absolute top-0 left-0 w-full h-fit">
+      <div
+        className=" absolute top-0 left-0 h-screen w-full -z-20"
+        id="vanta"
+      ></div>
+      <Particle />
       <Nav />
-      {/* <Principal /> */}
+      <Present />
+      {/* <Principal />
       <Crew />
-      <Present duration={5 * 24 * 60 * 60 * 1000} />
       <About />
       <Carousel />
-      {/* <Gallery
+      <Gallery
         setChildVisible={setChildVisible}
         setImg={setImg}
         setTag={setTag}
-      /> */}
+      />
       <Convenors />
       <Secretary />
       <Contact />
-      {/* {childVisible && (
+      {childVisible && (
         <ViewGalleryImage
           img={img}
           tag={tag}
@@ -51,5 +73,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
--- client-app/src/Homepage/Present.jsx
+++ client-app/src/Homepage/Present.jsx
@@ -1,16 +1,18 @@
-import { Timer } from "../Timer";
-
 const Present = ({ duration }) => {
   return (
     <div className="relative flex flex-col space-y-5 ml-6 mr-6 mb-4">
-      <h1 className="text-5xl font-semibold tracking-wide text-yellow-600">
+      <h1 className="text-5xl font-semibold tracking-wide text-orange-400">
         BENGAL INSTITUTE OF TECHNOLOGY
       </h1>
       <h1 className="text-4xl text-sky-500 font-custom-cursive">Presents</h1>
       <div className="flex justify-center">
-        <img src="logo.png" alt="bits2Bytes2k23 Logo" className="w-1/4 logo-img hover-rotate hover:scale-100" />
+        <img
+          src="logo.png"
+          alt="bits2Bytes2k23 Logo"
+          className=" w-96 logo-img -ml-28 -mr-4 z-10"
+        />
+        <div className="rounded-full bg-black h-40 w-40 mt-24 -ml-64"></div>
       </div>
-      <Timer duration={duration} />
     </div>
   );
 };
