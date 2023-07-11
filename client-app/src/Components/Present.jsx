e584ddcc258e6215613005341a7f305684678781
Priyankar Sarkar <priyankarsarkar2020@gmail.com>
2023-07-12 01:24:54 +0530

    present component logo bug fixed

--- client-app/src/Components/Present.jsx
+++ client-app/src/Components/Present.jsx
@@ -5,13 +5,13 @@ const Present = () => {
         BENGAL INSTITUTE OF TECHNOLOGY
       </h1>
       <h1 className="text-4xl text-sky-500 font-custom-cursive">Presents</h1>
-      <div className="flex justify-center">
+      <div className="flex justify-center items-center">
         <img
           src="logo.png"
           alt="bits2Bytes2k23 Logo"
-          className="w-72 md:w-80 2xl:w-96 logo-img -ml-28 -mr-4 z-10"
+          className="w-72 md:w-80 2xl:w-96 logo-img z-20"
         />
-        <div className="rounded-full bg-black h-40 w-40 mt-24 -ml-64"></div>
+        <div className="rounded-full bg-black h-28 w-28 mt-24 absolute z-10 -translate-y-1/2"></div>
       </div>
     </div>
   );
