6be6ecab7748397838186a12a982ce933739e7fa
Priyankar Sarkar <priyankarsarkar2020@gmail.com>
2023-07-31 13:36:07 +0530

    .venv removed

--- client-app/src/main.jsx
+++ client-app/src/main.jsx
@@ -3,14 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import ScrollTop from "./Components/ScrollTop.jsx";
-import { CookiesProvider } from "react-cookie";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <CookiesProvider>
-      <ScrollTop />
       <App />
-    </CookiesProvider>
   </BrowserRouter>
 );
